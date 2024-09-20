03e73290a7afd1341500aa8f89007b7bea9412da
kartikay <kartikay0502@gmail.com>
2024-09-20 16:06:35 +0530

    Updated Submodule

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [posts,setPosts] = useState([])
+  const [title,setTitle] = useState('')
+  const [content,setContent] = useState('')
+
+  useEffect(()=>{
+    fetchPosts()
+  },[])
+
+  const fetchPosts = async()=>{
+    const response = await axios.get('http://localhost:5001/posts');
+    setPosts(response.data)
+  }
+
+  const addPost = async(e)=>{
+    e.preventDefault()
+    try{
+      const response = await axios.post('http://localhost:5001/posts',{title,content})
+      setPosts([...posts,response.data])
+      setTitle('')
+      setContent('')
+    }
+    catch(error){
+        console.log('error',error)
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Blog Post</h1>
+        <form onSubmit={addPost}>
+          <input type='text' placeholder='Ttile' value={title} onChange={(e)=>{setTitle(e.target.value)}} />
+          <textarea placeholder='content' value={content} onChange={(e)=>{setContent(e.target.value)}} required/>
+            <button type='submit'>Post</button>
+        </form>
+        <ul>
+          {posts.map((posts)=>(
+            <li key={posts.id}>
+              <h2>{posts.title}</h2>
+              <p>{posts.content}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
